5f27eb5609074ff42ac3300637dbf659eaefa3f3
Ankit Negi <73025934+nankit793@users.noreply.github.com>
2022-04-08 20:59:43 +0530

    changes to be seeen

--- backend/models/DownlodCV.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose;
-
-const DownlodCV = new Schema({
-    link:  String
-})
-module.exports = mongoose.model('downlaodcv', DownlodCV);
--- backend/models/Hire.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose;
-
-const Hire = new Schema({
-    name: String,
-    email: String, 
-    message: String
-})
-module.exports = mongoose.model('hire', Hire);
